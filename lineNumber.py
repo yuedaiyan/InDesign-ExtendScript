cd4d75eb9b90312f317929c04fa6e588b85a19cb
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-05-19 20:58:18 +0800

    加入计算行号计算并复制到剪切板函数

---
+++ lineNumber.py
@@ -0,0 +1,14 @@
+import pyperclip
+
+# 在这里调整两个数字
+start = 1031
+end = 1350
+
+# 生成数列(包含 start 和 end)
+numbers = "\n".join(str(i) for i in range(start, end + 1))
+
+# 复制到剪贴板
+pyperclip.copy(numbers)
+
+print(f"已将 {start} 到 {end} 的数列复制到剪贴板:")
+print(numbers)
